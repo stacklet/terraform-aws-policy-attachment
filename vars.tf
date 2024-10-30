f25a89c3dfea3b73d0429a153b826a9a42a7fcdd
John Anderson <sontek@gmail.com>
2024-10-30 17:04:42 -0400

    add validation

--- vars.tf
+++ vars.tf
@@ -1,6 +1,11 @@
 variable "resource_type" {
-  description = "Type of resource to which the policy applies, e.g., 's3_bucket' or 'secrets_manager_secret'"
+  description = "Type of resource to which the policy applies, e.g., 'aws_s3_bucket' or 'aws_secretsmanager_secret'"
   type        = string
+
+  validation {
+    condition     = contains(["aws_s3_bucket", "aws_secretsmanager_secret"], var.resource_type)
+    error_message = "resource_type must be one of 'aws_s3_bucket' or 'aws_secretsmanager_secret'."
+  }
 }
 
 variable "resource_id" {
